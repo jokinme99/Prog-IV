34ae562e4ca799f4511309ed630ca7f0c8b44208
AITOR <AITOR@192.168.1.110>
2020-03-29 14:42:34 +0200

    registro

--- E-restaurants/usuario/Usuario.h
+++ E-restaurants/usuario/Usuario.h
@@ -8,15 +8,23 @@
 #ifndef USUARIO_USUARIO_H_
 #define USUARIO_USUARIO_H_
 
+typedef struct
+{
+	char contraseña[30];
+	char usuario[30];
+	char contraseña[30];
+	int admin;
+
+}cuenta;
+
 typedef struct {
 	char nom[10];
 	char correo[20];
-	char contra[15];
 	int numTarjeta;
 	float saldo;
 }Usuario;
 
-Usuario nuevoCliente() // no sé si está bien!!
+Usuario nuevoCliente() // no sé si está bien!! esto para que????
 
 
 #endif /* USUARIO_USUARIO_H_ */
