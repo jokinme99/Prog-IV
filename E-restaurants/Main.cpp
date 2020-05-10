5ae9769c12673b08ff4e8a46f092dbc5190afee2
mikelgomez <mikelgomez@opendeusto.es>
2020-05-10 20:05:13 +0200

    Añadido la creación de un administrador guardando los datos en txt y el login leyendo los datos

--- E-restaurants/Main.cpp
+++ E-restaurants/Main.cpp
@@ -11,10 +11,11 @@
 #include <string.h>
 #include "Restaurante.h"
 #include <cstdlib>
+#include <fstream>
 
 #include "Menu.h"
-#define ADMIN "admin"
-#define PASSWORD "admin"
+//#define ADMIN "admin"
+//#define PASSWORD "admin"
 #define USUARIO "usuario"
 #define CONTRA "usuario"
 using namespace std;
@@ -23,7 +24,18 @@ using namespace std;
 int main(void)
 {
 
-		int opcion,salir,opcionAdmin,opcionUsuario;
+		int opcion,salir,opcionAdmin,opcionUsuario,op;
+
+		//parte de crear el administrador y login del administrador
+		string nombreAdmin;
+		string contraAdmin;
+		string linea;
+		string no;
+		string co;
+		int con = 0;
+		string parte;
+
+
 		string usuario;
 		string contrasenya;
 		cout<<"-----BIENVENIDO A E-RESTAURANTS-----"<<endl;
@@ -34,29 +46,95 @@ int main(void)
 
 		switch(opcion){
 		case 1:
-			cout<<"Introducir el usuario: ";
-			cin>>usuario;
-			cout<<"Introducir contrasenya: ";
-			cin>>contrasenya;
-			if(usuario == ADMIN && contrasenya == PASSWORD){
-				cout<<"---MODO ADMINISTRADOR---"<<endl;
-				cout<<"1. Ver todos los restaurantes"<<endl;
-				cout<<"2. Modificar restaurantes"<<endl;
-				cout<<"3. Anyadir restaurante"<<endl;
-				cout<<"4.Eliminar restaurante"<<endl;
-				cin>>opcionAdmin;
-				switch(opcionAdmin){
-				case 1:
-					system("cls");
-					cout<<"opcion 1";
-					system("pause");
-					system("cls");
-					break;
-				}
-			}else{
-				cout<<"Contrasenya y/o usuario incorrectos"<<endl;
-				return main();
+			cout << "1. Registrar administrador" << endl;
+			cout << "2. Iniciar como administrador" << endl;
+			cout << "3. Salir" << endl;
+			cin >> op;
+
+			if(op == 1){
+				cout << "Ingresa el nombre del admin\n";
+				cin >> nombreAdmin;
+				cout << endl;
+
+				cout << "Ingresa la contraseña del admin\n";
+				cin >> contraAdmin;
+				cout << endl;
+
+
+				ofstream ofs("prueba.txt");
+
+				ofs << nombreAdmin << "," << contraAdmin;
+
+				ofs.close();
+
+				cout << "Administrador creado correctamente" << endl;
+
 			}
+
+			if(op == 2){
+				cout << "Ingresa el nombre del admin\n";
+				cin >> nombreAdmin;
+				cout << endl;
+
+				cout << "Ingresa la contraseña del admin\n";
+				cin >> contraAdmin;
+				cout << endl;
+
+				ifstream ifs("prueba.txt");
+
+				while(!ifs.eof()){
+				ifs >> linea;
+
+
+
+				char separador = ',';
+				for(size_t p=0, q=0; p!=linea.npos;p=q){
+					parte= linea.substr(p+(p!=0), (q=linea.find(separador,p+1))-p-(p!=0));
+						if(con == 0){
+							no = parte;
+							//cout << "El nombre es: " << no << endl;
+						}else if(con == 1){
+							co = parte;
+							//cout << "La contra es: " << co << endl;
+						}else{
+							cout << "error";
+						}
+						con++;
+				}
+
+				ifs.close();
+
+				}
+				if(nombreAdmin == no && contraAdmin == co){
+					cout << "Nombre y contra introduccidos correctamente" << endl;
+					cout << endl;
+					/*cout << "nombre introduccido: " << nombreAdmin << endl; // esta linea y las 3 siguientes es para ver si funciona bien, quitar para la entrega final!
+					cout << "nombre del txt: " << no << endl;
+					cout << "contra introduccido: " << contraAdmin << endl;
+					cout << "contra del txt: " << co << endl;*/
+
+					//menu del admin
+					cout<<"---MODO ADMINISTRADOR---"<<endl;
+					cout<<"1. Ver todos los restaurantes"<<endl;
+					cout<<"2. Modificar restaurantes"<<endl;
+					cout<<"3. Anyadir restaurante"<<endl;
+					cout<<"4.Eliminar restaurante"<<endl;
+					cin>>opcionAdmin;
+					switch(opcionAdmin){
+						case 1:
+						system("cls");
+						cout<<"opcion 1";
+						system("pause");
+						system("cls");
+						break;
+					}
+
+
+					}else{
+						cout << "El nombre o la contra introduccidos son incorrectos" << endl;
+					}
+
+				}
 
 		break;
 		case 2:
